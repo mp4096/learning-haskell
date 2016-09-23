ab806cd127563d6da5c250c1034efb89289ce487
Mikhail Pak <mikhail.pak@tum.de>
2016-09-24 00:03:06 +0200

    Ugly, but works

---
+++ 2016-09_FutureLearn/Week_02/starman.hs
@@ -0,0 +1,24 @@
+-- Replace letters by dashes if not correctly guessed
+makeSecretExcept :: String -> String -> String
+makeSecretExcept secret guesses =
+    [if elem letter guesses then letter else '-' | letter <- secret]
+
+-- Check if the user has won
+hasWon :: String -> String -> Bool
+hasWon secret guesses = all (\l -> elem l guesses) secret
+
+mainLoop :: String -> Int -> Int -> String -> IO ()
+mainLoop secret maxLives currLives guesses = do
+    putStrLn ((show currLives) ++ " of " ++ (show maxLives))
+    if currLives == 0
+    then putStrLn "Game over"
+    else do
+        putStrLn (makeSecretExcept secret guesses)
+        putStr "Your guess: "
+        line <- getLine
+        let newGuesses = head line : guesses
+        if hasWon secret newGuesses
+        then do { putStrLn secret; putStrLn "You won!" }
+        else mainLoop secret maxLives (if elem (head line) secret
+                                       then currLives
+                                       else currLives - 1) newGuesses
